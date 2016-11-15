04c41e88a2ef6b8712a895ff825576bb054293ab
simon-baehler <simon-baehler@heig-vd.ch>
2016-11-15 16:03:24 +0100

    java doc

--- projet/src/main/java/app/controller/BadgeController.java
+++ projet/src/main/java/app/controller/BadgeController.java
@@ -24,8 +24,15 @@ public class BadgeController {
     LinkedList<Badge> badgesList = new LinkedList<Badge>();
     int count = 0;
 
+    /**
+     * retourne le badge passé en parametre
+     *
+     * @date 14 Nov 2016
+     * @param id
+     * @return le badge avec l'id en param, null si rien n'est trouvé
+     */
      @RequestMapping("/badge")
-    public Badge badge(@RequestParam(value="id") int id) {
+    public Badge badge(@RequestParam int id) {
          for(int i = 0; i < badgesList.size(); i++) {
             if(badgesList.get(i).getId() == id)
             {
@@ -35,17 +42,40 @@ public class BadgeController {
          return null;
     }
 
+
+    /**
+     * retourne tous les bages
+     *
+     * @date 15 Nov 2016
+     * @param -
+     * @return retourne tous les badges
+     */
     @RequestMapping("/badges")
     public LinkedList badges() {
         return badgesList;
     }
 
+    /**
+     * post et retourne le badge passé en parametre
+     *
+     * @date 15 Nov 2016
+     * @param name desc image
+     * @return lpost et retourne badge avec l'id en param, null si rien n'est trouvé
+     */
     @RequestMapping(value = "/badge", method = RequestMethod.POST)
     public Badge doPost(@RequestParam String name, String desc, String image) {
         Badge b = new Badge(++count, name, desc, image);
         badgesList.add(b);
         return b;
     }
+
+    /**
+     * modifie le badge passé en parametre
+     *
+     * @date 15 Nov 2016
+     * @param name desc image
+     * @return modifie et retourne badge avec l'id en param, null si rien n'est trouvé
+     */
     @RequestMapping(value = "/badge", method = RequestMethod.PUT)
     public Badge doLPut(@RequestParam int id, String name, String desc, String image) {
         for(int i = 0; i < badgesList.size(); i++) {
