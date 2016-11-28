897bd9177a5af75634e702dd8ad7a8eeb59b5670
CmtCousteau <marco.monzione@gmail.com>
2016-11-28 16:12:33 +0100

    marche*

--- test/spring-server/src/main/java/ch/heigvd/gamification/api/BadgesEndpoint.java
+++ test/spring-server/src/main/java/ch/heigvd/gamification/api/BadgesEndpoint.java
@@ -5,10 +5,26 @@
  */
 package ch.heigvd.gamification.api;
 
+import ch.heigvd.gamification.api.dto.Badges;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author marco
  */
-public class BadgesEndpoint {
+@RestController
+public class BadgesEndpoint implements BadgesApi {
+
+    @Override
+    public ResponseEntity<List<Badges>> badgesGet() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ResponseEntity<Void> badgesPost(Badges badge) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
