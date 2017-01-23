35653acdc2dac97030cf9200cfd8e00c7520a931
Jean <jean.ayoub@heig-vd.ch>
2017-01-23 23:16:54 +0100

    Basic application CRUD endpoints tests are done

--- Teaching-HEIGVD-AMT-2016-Gamification/ExecutableSpecification/src/test/java/ch/heigvd/amt/gamification/spec/steps/ApplicationSteps.java
+++ Teaching-HEIGVD-AMT-2016-Gamification/ExecutableSpecification/src/test/java/ch/heigvd/amt/gamification/spec/steps/ApplicationSteps.java
@@ -11,7 +11,6 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import static org.junit.Assert.*;
@@ -39,7 +38,7 @@ public class ApplicationSteps {
     
     @Then("^I receive a (\\d+) status code$")
     public void i_receive_a_status_code(int arg1) throws Throwable {
-        assertEquals(arg1, response.getStatusCode());
+        assertEquals(arg1, statusCode);
     }
     
     
@@ -51,6 +50,7 @@ public class ApplicationSteps {
             
           
         } catch (ApiException e) {
+            statusCode = e.getCode();
             System.err.println(e.getCause());
         }
         assertEquals(200, statusCode);
@@ -67,48 +67,34 @@ public class ApplicationSteps {
         
         try {
             response = api.applicationsPostWithHttpInfo(applicationPost);
-            //String location = (String)response.getHeaders().get("location");
+            statusCode = response.getStatusCode();
             Map map = (Map)response.getHeaders();
             String s = map.values().toString();
             s = s.substring(41);
-            String [] l = s.split("]");
-            id = Long.valueOf(location.substring(39));
-             System.err.println("nelfnefnekwwnfewlnfewnlfnewldcnkldscnjdcjkdnscdjcdncjfnewfknew: " + s);
-            statusCode = response.getStatusCode();
-            //statusCode = api.applicationsPostWithHttpInfo(applicationPost).getStatusCode();
+            String [] list = s.split("]");
+            id = Long.valueOf(list[0]);
+            //System.err.println("nelfnefnekwwnfewlnfewnlfnewldcnkldscnjdcjkdnewfknew: " + statusCode);
         } catch (ApiException e) {
+            statusCode = e.getCode();
             System.err.println(e.getCode());
         }
         assertEquals(201, statusCode);
     }
     
-
-    @When("^I ask for the application with a GET on the /applications/id endpoint$")
-    public void I_ask_for_the_application_with_a_GET_on_the_applications_id_endpoint() 
-            throws Throwable {
-        try {
-            //response = api.applicationsIdGetWithHttpInfo(id);
-            statusCode = api.applicationsIdGetWithHttpInfo(id).getStatusCode();
-        } catch (ApiException e) {
-            System.err.println(e.getCause());
-        }
-    }
-    
-    
-    @When("^I ask for a list of registered apps with a GET on the /applications endpoint$")
-    public void i_ask_for_a_list_of_registered_apps_with_a_GET_on_the_registrations_endpoint() throws Throwable {
-        try {
-            applications = api.applicationsGet();
-            //response = api.applicationsGetWithHttpInfo();
-            statusCode = api.applicationsGetWithHttpInfo().getStatusCode();
-        } catch (ApiException e) {
-            System.err.println(e.getCause());
-        }
-    }
     /***************************************************************************
      **************************************************************************/
     
     
+    
+    
+
+   
+
+
+    
+    /***************************************************************************
+     * applicationRegistration
+     **************************************************************************/
     
     @Given("^I have an application payload$")
     public void i_have_an_application_payload() throws Throwable {
@@ -120,9 +106,16 @@ public class ApplicationSteps {
     @When("^I POST it to the /applications endpoint$")
     public void i_POST_it_to_the_applications_endpoint() throws Throwable {
         try {
+            response = api.applicationsPostWithHttpInfo(applicationPost);
+            statusCode = response.getStatusCode();
+            Map map = (Map)response.getHeaders();
+            String s = map.values().toString();
+            s = s.substring(41);
+            String [] list = s.split("]");
+            id = Long.valueOf(list[0]);
             //response = api.applicationsPostWithHttpInfo(applicationPost);
-            statusCode = api.applicationsPostWithHttpInfo(applicationPost).getStatusCode();
         } catch (ApiException e) {
+            statusCode = e.getCode();
             System.err.println(e.getCode());
         }
     }
@@ -134,31 +127,12 @@ public class ApplicationSteps {
         expected.setApplicationName(applicationPost.getName());
         assertThat(applications).extracting("name").contains(applicationPost.getName());
     }
-
-   
-
-    @Given("^I have an existing application ID with a different payload$")
-    public void i_have_an_existing_application_ID_with_a_different_payload() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-
-    @Then("^I don't see my app in the list$")
-    public void i_don_t_see_my_app_in_the_list() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-
-
-    
-    /***************************************************************************
-     * applicationRegistration
-     **************************************************************************/
-    
      /***************************************************************************
      **************************************************************************/
+    
+    
+    
+    
     
     
     
@@ -182,19 +156,22 @@ public class ApplicationSteps {
      
     @When("^I PUT it to the /applications/id endpoint$")
     public void i_PUT_it_to_the_applications_id_endpoint() throws Throwable {
-
-        try {         
+        
+        
+        try {    
+            /*
             applications = api.applicationsGet();
             for (ApplicationGet app : applications) {
                 if (app.getName().equals("PutTest")) {
                     id = app.getId();
                 }
             }
-            
+        */
             //response = api.applicationsIdPutWithHttpInfo(id, applicationPost);
             statusCode = api.applicationsIdPutWithHttpInfo(id, applicationPost).getStatusCode();
             api.applicationsIdPut(id, applicationPost);
         } catch (ApiException e) {
+            statusCode = e.getCode();
             System.err.println(e.getCode());
         }
     }
@@ -202,14 +179,26 @@ public class ApplicationSteps {
     
     @Then("^I see that the modifications took effect$")
     public void i_see_that_the_modifications_took_effect() throws Throwable {
-        LinkedTreeMap ltp = (LinkedTreeMap) api.applicationsIdGet(id);
-        String description = (String)ltp.get("description");
-        
+         String description = null;
+        try {
+            LinkedTreeMap ltp = (LinkedTreeMap) api.applicationsIdGet(id);
+            description = (String)ltp.get("description");
+        } catch (ApiException e) {
+            statusCode = e.getCode();
+            System.err.println(e.getCause());
+        }
         assertEquals("PutTest_Description_Success", description);
     }
     
     /***************************************************************************
      **************************************************************************/
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -222,7 +211,9 @@ public class ApplicationSteps {
         try {
             //response = api.applicationsIdDeleteWithHttpInfo(id);
             statusCode = api.applicationsIdDeleteWithHttpInfo(id).getStatusCode();
+            System.err.println("nelfnefnekwwnfewlnfewnlfnewldcnkldscnjdcjkdnewfknew: " + statusCode);
         } catch (ApiException e) {
+            statusCode = e.getCode();
             System.err.println(e.getCause());
         }
     }
@@ -238,10 +229,37 @@ public class ApplicationSteps {
     
 
     
+    
+    
+    
     /***************************************************************************
      * applicationReading
      **************************************************************************/
     
+     @When("^I ask for the application with a GET on the /applications/id endpoint$")
+    public void I_ask_for_the_application_with_a_GET_on_the_applications_id_endpoint() 
+            throws Throwable {
+        try {
+            //response = api.applicationsIdGetWithHttpInfo(id);
+            statusCode = api.applicationsIdGetWithHttpInfo(id).getStatusCode();
+        } catch (ApiException e) {
+            statusCode = e.getCode();
+            System.err.println(e.getCause());
+        }
+    }
+    
+    
+    @When("^I ask for a list of registered apps with a GET on the /applications endpoint$")
+    public void i_ask_for_a_list_of_registered_apps_with_a_GET_on_the_registrations_endpoint() throws Throwable {
+        try {
+            applications = api.applicationsGet();
+            //response = api.applicationsGetWithHttpInfo();
+            statusCode = api.applicationsGetWithHttpInfo().getStatusCode();
+        } catch (ApiException e) {
+            statusCode = e.getCode();
+            System.err.println(e.getCause());
+        }
+    }
     
    /***************************************************************************
     **************************************************************************/ 
--- Teaching-HEIGVD-AMT-2016-Gamification/ExecutableSpecification/src/test/resources/scenarios/applicationsRegistration.feature
+++ Teaching-HEIGVD-AMT-2016-Gamification/ExecutableSpecification/src/test/resources/scenarios/applicationsRegistration.feature
@@ -1,24 +1,24 @@
 # some comment
 # language: en
 
-#Feature: Application registration
+Feature: Application registration
 
-#  Background:
-#   Given I have an application payload 
+  Background:
+   Given I have an application payload 
 
-#  Scenario: Post a new application
-#   Given I have an application payload
-#   When I POST it to the /applications endpoint
-#   Then I receive a 201 status code
+  Scenario: Post a new application
+   Given I have an application payload
+   When I POST it to the /applications endpoint
+   Then I receive a 201 status code
 
-#  Scenario: Check that the application has been posted
-#   Given I have an application payload
-#   When I POST it to the /applications endpoint
-#   And I ask for a list of registered apps with a GET on the /applications endpoint
-#   Then I see my app in the list
+  Scenario: Check that the application has been posted
+   Given I have an application payload
+   When I POST it to the /applications endpoint
+   And I ask for a list of registered apps with a GET on the /applications endpoint
+   Then I see my app in the list
 
-#  Scenario: Check that it is not possible to create two apps with the same name
-#   Given I have an application payload
-#   When I POST it to the /applications endpoint
-#   And I POST it to the /applications endpoint
-#   Then I receive a 409 status code
+  Scenario: Check that it is not possible to create two apps with the same name
+   Given I have an application payload
+   When I POST it to the /applications endpoint
+   And I POST it to the /applications endpoint
+   Then I receive a 409 status code
